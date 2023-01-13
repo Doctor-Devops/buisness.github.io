27de593cfbdd85504848669fe192bf003aa912f3
vikas17a <vikas17a@gmail.com>
2023-01-13 05:40:51 +0100

    Add google analytics

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: HeroBiz - v2.4.0
-* Template URL: https://bootstrapmade.com/herobiz-bootstrap-business-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
